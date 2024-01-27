e404029325e38b0cfe0041eaaef85c7e492059c0
Tebbo <leetebbs@ymail.com>
2024-01-27 17:23:22 +0000

    lesson 2 build a paymaster

---
+++ scripts/deploy.js
@@ -0,0 +1,26 @@
+const hre = require("hardhat");
+
+async function main() {
+  const af = await hre.ethers.deployContract("AccountFactory");
+
+  await af.waitForDeployment();
+
+  console.log(`AccountFactory deployed to ${af.target}`);
+
+  const ep = await hre.ethers.deployContract("EntryPoint");
+
+  await ep.waitForDeployment();
+
+  console.log(`EntryPoint deployed to ${ep.target}`);
+
+  const pm = await hre.ethers.deployContract("Paymaster");
+
+  await pm.waitForDeployment();
+
+  console.log(`Paymaster deployed to ${pm.target}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
---
+++ scripts/deployEP.js
@@ -0,0 +1,14 @@
+const hre = require("hardhat");
+
+async function main() {
+  const ep = await hre.ethers.deployContract("EntryPoint");
+
+  await ep.waitForDeployment();
+
+  console.log(`EntryPoint deployed to ${ep.target}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
